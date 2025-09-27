fccaaeb575148a9db0397eaec8d590f3345bed2d
ercnbayer <emre.bayer@hotmail.com>
2025-09-27 20:36:27 +0300

    unnecessary log removed

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -67,7 +67,7 @@ public class UserService : IUserService
     public async Task<Result<User>> GetUserByEmailAsync(string email)
     {
         var dbUserResult = await _userRepository.GetByEmailAsync(email);
-        Console.WriteLine(dbUserResult.Data.Salt);
+       
         if (!dbUserResult.IsSuccess)
         {
             return Result<User>.Fail(dbUserResult.Error);
